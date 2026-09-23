d921ae187e1d5c93b716f6698faa145ed82d1b84
moises-linux <navas.20moi@gmail.com>
2026-09-23 16:15:48 -0500

    shows succes mesage and dismiss button behavior

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ const successSection = document.getElementById("success-section");
 const successEmail = document.getElementById("success-email");
 const dismissBtn = document.getElementById("dismiss-button");
 const emailErrorMessage = document.getElementById("email-error");
-const isValidEmail = /^\S+@\S+$/;
+const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+//submit event handler
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  //creates object from input values
+  const { email } = Object.fromEntries(new FormData(form)); // email = Object.email
+  const cleanedEmail = (email || "").trim(); // remove whitespaces
 
-  const { email } = Object.fromEntries(new FormData(form));
-  const cleanedEmail = (email || "").trim();
-
+  //checks email is not falsy
   if (!cleanedEmail) {
     console.log("email required");
     emailErrorMessage.hidden = false;
@@ -23,11 +25,27 @@ form.addEventListener("submit", (e) => {
     emailInput.ariaInvalid = false;
   }
 
+  //checks email contains characters before and after the @ symbol
   if (!isValidEmail.test(cleanedEmail)) {
     console.log("valid email required");
     emailErrorMessage.hidden = false;
     emailInput.ariaInvalid = true;
     return;
   }
-  emailInput.value = "";
+
+  //Shows the success message
+  successEmail.textContent = cleanedEmail;
+  signupSection.hidden = true;
+  successSection.hidden = false;
+});
+
+//dismiss behavior event handler
+dismissBtn.addEventListener("click", () => {
+  successSection.hidden = true;
+  signupSection.hidden = false;
+  emailInput.value = ""; //Clear the input value.
+  //Hide the error and mark the input as valid again.
+  emailErrorMessage.hidden = true;
+  emailInput.setAttribute("aria-invalid", "false");
+  emailInput.focus(); // keyboard and screen-reader users return directly to the form input
 });
