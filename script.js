9eb40303f943718da046f423c53d7f33476e7471
moises-linux <navas.20moi@gmail.com>
2026-09-23 01:40:37 -0500

    Refactor email validation logic in sign-up form submission

--- script.js
+++ script.js
@@ -7,31 +7,27 @@ const dismissBtn = document.getElementById("dismiss-button");
 const emailErrorMessage = document.getElementById("email-error");
 const isValidEmail = /^\S+@\S+$/;
 
-//submit listener
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const formData = new FormData(e.target); //  converts the the form element into a collection of key / value pairs
-  let data = Object.fromEntries(formData); // converts these entries into an object
 
-  for (const key in data) {
-    // removes whitespace from email
-    const email = data[key].trim();
+  const { email } = Object.fromEntries(new FormData(form));
+  const cleanedEmail = (email || "").trim();
 
-    //email validation
-    if (!email) {
-      console.log("email required");
-      emailErrorMessage.hidden = false;
-      emailInput.ariaInvalid = true;
-      return;
-    } else {
-      emailErrorMessage.hidden = true;
-      emailInput.ariaInvalid = false;
-    }
-
-    //email contains characters before and after the @ symbol
-    if (!isValidEmail.test(email)) {
-      console.log(" valid email required");
-      return;
-    }
+  if (!cleanedEmail) {
+    console.log("email required");
+    emailErrorMessage.hidden = false;
+    emailInput.ariaInvalid = true;
+    return;
+  } else {
+    emailErrorMessage.hidden = true;
+    emailInput.ariaInvalid = false;
   }
+
+  if (!isValidEmail.test(cleanedEmail)) {
+    console.log("valid email required");
+    emailErrorMessage.hidden = false;
+    emailInput.ariaInvalid = true;
+    return;
+  }
+  emailInput.value = "";
 });
